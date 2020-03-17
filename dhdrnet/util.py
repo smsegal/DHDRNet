c8d251eefa1aeeaa278b722997123a195b5056cb
Shane Segal <shane@smsegal.ca>
2020-03-17 15:50:39 -0400

    train test split

--- dhdrnet/util.py
+++ dhdrnet/util.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from subprocess import check_output
 import torch
 import numpy as np
+from typing import Set
 
 
 def get_project_root() -> Path:
@@ -20,11 +21,23 @@ def get_train_test(data_dir: Path) -> torch.utils.data.Dataset:
 
 
 def create_train_test_split(data_dir: Path, train_split=0.9, val_split=0.2):
-    files = set((data_dir / "dngs").iterdir())
-    train = random.sample(files, k=train_split * len(files))
+    files: Set = set((data_dir / "dngs").iterdir())
+
+    train_size = int(train_split * len(files))
+    train: Set = set(random.sample(files, k=train_size))
     test = files - train
-    val = random.sample(train, k=val_split * len(train))
+
+    val_size = int(val_split * len(train))
+    val = set(random.sample(train, k=val_size))
+    train = train - val
+
+    print(len(files))
+    print(len(train))
+    print(len(test))
+    print(len(val))
+    assert len(files) == len(train) + len(test) + len(val)
+
     for name, split in {"train": train, "test": test, "val": val}.items():
         with open(f"{name}.txt", "w") as f:
             for fp in split:
-                print(str(fp), file=f)
+                print(fp.name, file=f)
