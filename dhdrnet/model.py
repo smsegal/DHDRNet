bdd5ff51e1f60e1fce6613271e693697ea39ce2e
Shane Segal <shane@smsegal.ca>
2020-03-25 19:33:56 -0400

    training loop almost going

--- dhdrnet/model.py
+++ dhdrnet/model.py
@@ -1,5 +1,7 @@
 import copy
 import time
+from enum import Enum
+from functools import partial
 from pathlib import Path
 
 import numpy as np
@@ -13,27 +15,60 @@ from torch.utils.data import DataLoader
 from torchvision import models
 
 from dhdrnet.Dataset import HDRDataset
-from dhdrnet.util import get_project_root
+from dhdrnet.image_loader import CVFuse, FuseMethod
+from dhdrnet.util import Indexable, get_project_root
 
 DATA_DIR = get_project_root() / "data"
-# dataloader =
+
+
+class Phase(Enum):
+    TRAIN = "train"
+    EVAL = "val"
+
+
+datasets = {
+    split: HDRDataset(
+        DATA_DIR / split.value / "merged", DATA_DIR / split.value / "dngs"
+    )
+    for split in Phase
+}
+dataloaders = {
+    split: torch.utils.data.DataLoader(
+        datasets[split],
+        batch_size=4,
+        shuffle=True,
+        num_workers=4,
+        sampler=torch.utils.data.RandomSampler,
+    )
+    for split in Phase
+}
+dataset_sizes = {x: len(datasets[x]) for x in Phase}
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 def main():
-    model = define_model()
+    model = models.resnet50(pretrained=True)
+    num_features = model.fc.in_features
+    num_classes = 5  # number of exposures
 
-    datasets = {
-        split: HDRDataset(DATA_DIR / split / "merged", DATA_DIR / split / "dngs")
-        for split in ["train", "val"]
-    }
-    dataloaders = {
-        split: torch.utils.data.DataLoader(
-            datasets[split], batch_size=4, shuffle=True, num_workers=4
-        )
-        for split in ["train", "val"]
-    }
+    model.fc = nn.Linear(num_features, num_classes)
 
-    train(model, None, None, None, 25)
+    fuser = CVFuse(FuseMethod.Mertens)
+    criterion = ReconstructionLoss(fuser)
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+
+    train(model, criterion, optimizer, exp_lr_scheduler, num_epochs=25)
+
+
+class ReconstructionLoss(nn.Module):
+    def __init__(self, fuse_fun):
+        super(ReconstructionLoss, self).__init__()
+        self.fuse_fun = fuse_fun
+
+    def forward(self, input, target):
+        pass
 
 
 def define_model(pretrained=True):
@@ -42,7 +77,7 @@ def define_model(pretrained=True):
     return resnext
 
 
-def train(model, loss, optimizer, scheduler, num_epochs):
+def train(model, loss_fun, optimizer, scheduler, num_epochs):
     since = time.time()
 
     best_weights = copy.deepcopy(model.state_dict())
@@ -52,17 +87,64 @@ def train(model, loss, optimizer, scheduler, num_epochs):
         print(f"Epoch {epoch}/{num_epochs - 1}")
         print("-" * 10)
 
-        for phase in ["train", "val"]:
-            if phase == "train":
+        for phase in Phase:
+            if phase == Phase.TRAIN:
                 model.train()  # set to training mode
             else:
                 model.eval()
 
-            running_loss = 0.0
-            running_correct = 0
+            # iterate over data
+            running_loss, running_correct = data_opt(
+                dataloaders, model, phase, loss_fun, device, optimizer
+            )
 
-            #iterate over data
-            for inputs, labels in dataloaders
+            if phase == Phase.TRAIN:
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_correct.double() / dataset_sizes[phase]
+
+            print(f"{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}")
+
+            # deep copy model if eval time
+            if phase == Phase.EVAL and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_weights = copy.deepcopy(model.state_dict())
+
+        print()  # line sep
+
+    time_elapsed = time.time() - since
+    print(f"Training complete in {time_elapsed // 60:.0f}m {time_elapsed %60:.0f}s")
+    print(f"Best val Acc: {best_acc:4f}")
+
+    model.load_state_dict(best_weights)
+    return model
+
+
+def data_opt(dataloaders, model, phase, loss_fun, device, optimizer):
+    running_loss = 0.0
+    running_correct = 0
+    for inputs, labels, names in dataloaders[phase]:
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        optimizer.zero_grad()
+
+        # forwards pass
+        with torch.set_grad_enabled(phase == Phase.TRAIN):
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            loss = loss_fun(outputs, labels)
+
+            # backwards + optim in training
+            if phase == Phase.TRAIN:
+                loss.backward()
+                optimizer.step()
+
+        # stats
+        running_loss += loss.item() * inputs.size(0)
+        running_correct += torch.sum(preds == labels.data)
+        return running_loss, running_correct
 
 
 if __name__ == "__main__":
