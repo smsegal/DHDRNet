0a73ef2fcce67cdc65b461cd27a2ff1f08cf5135
Shane Segal <shane@smsegal.ca>
2020-07-07 12:52:47 -0400

    training updates

--- dhdrnet/lightning_train.py
+++ dhdrnet/lightning_train.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from pytorch_lightning import Trainer, loggers
 from pytorch_lightning.callbacks import ModelCheckpoint
-
+import datetime
 from dhdrnet.lightning_model import DHDRNet
 from dhdrnet.model_lut import DHDRNet as LUTNet
 
@@ -14,7 +14,7 @@ def main(hparams=None):
     elif hparams.method == "reconstruction":
         Model = DHDRNet
 
-    if (checkpoint_path := Path(hparams.checkpoint_path)).exists():
+    if checkpoint_path := hparams.checkpoint_path:
         model = Model.load_from_checkpoint(checkpoint_path=str(checkpoint_path))
     else:
         model = Model()
@@ -25,15 +25,20 @@ def main(hparams=None):
         monitor="val_loss",
         mode="min",
         save_top_k=3,
-        filepath="checkpoints/dhdr_{method}-{epoch}-{val_loss:.2f}-{val_ssim:.2f}",
+        filepath="checkpoints/dhdr_{method}-{epoch}-{val_loss:.2f}",
     )
 
-    trainer = Trainer(
-        gpus=hparams.gpus, logger=logger, checkpoint_callback=checkpoint_loss_callback
-    )
+    if checkpoint_path and not hparams.test_only:
+        trainer = Trainer(gpus=hparams.gpus, logger=logger, checkpoint_callback=checkpoint_loss_callback,
+                          resume_from_checkpoint=checkpoint_path)
+    else:
+        trainer = Trainer(
+            gpus=hparams.gpus, logger=logger, checkpoint_callback=checkpoint_loss_callback, max_epochs=2000
+        )
 
     if not hparams.test_only:
         trainer.fit(model)
+        trainer.save_checkpoint(f"checkpoints/dhdr_final{datetime.datetime.now().isoformat()}.ckpt")
 
     trainer.test(model)
 
@@ -43,7 +48,7 @@ if __name__ == "__main__":
     parser.add_argument("--gpus", default=None)
     parser.add_argument("--method", choices=["reconstruction", "lut"])
     parser.add_argument("--test-only", action="store_true")
-    parser.add_argument("-c", "--checkpoint-path")
+    parser.add_argument("-c", "--checkpoint-path", default=None)
     args = parser.parse_args()
     print(args)
     main(args)
