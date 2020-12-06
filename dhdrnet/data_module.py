5fa088832ef8c8c64e6795dd31fd19bef5f70dcd
Shane Segal <shane@smsegal.ca>
2020-12-06 17:06:03 -0500

    add data_module

---
+++ dhdrnet/data_module.py
@@ -0,0 +1,78 @@
+from pytorch_lightning import LightningDataModule
+from math import ceil
+from torch.utils.data import DataLoader, random_split
+from torchvision import transforms
+from dhdrnet.util import DATA_DIR, ROOT_DIR
+from dhdrnet.Dataset import LUTDataset
+from pathlib import Path
+from typing import List, Optional, Union
+
+import pandas as pd
+
+
+class HDRDataModule(LightningDataModule):
+    def __init__(
+        self,
+        data_dir: Path = DATA_DIR,
+        precomputed_data_dir: Path = ROOT_DIR / "precomputed_data",
+        batch_size: int = 20,
+    ):
+        super.__init__()
+        self.data_dir = data_dir
+        self.precomputed_data_dir = precomputed_data_dir
+        self.batch_size = batch_size
+        self.train_val_ratio = 0.9
+        self.transform = transforms.Compose(
+            [
+                transforms.Resize((300, 300)),
+                transforms.ToTensor(),
+            ]
+        )
+
+    def setup(self, stage: Optional[str] = None):
+        self.df: pd.DataFrame = pd.read_csv(
+            self.precomputed_data_dir / "store_current.csv"
+        )
+
+        if stage == "fit" or stage is None:
+            hdr_full = LUTDataset(
+                df=self.df,
+                exposure_path=(self.data_dir / "correct_exposures" / "exposures"),
+                raw_dir=self.data_dir / "dngs",
+                name_list=self.precomputed_data_dir / "train_current.csv",
+                transform=self.transform,
+            )
+            train_len = ceil(self.train_val_ratio * len(hdr_full))
+            self.hdr_train, self.hdr_val = random_split(
+                hdr_full, lengths=(train_len, len(hdr_full) - train_len)
+            )
+            self.dims = tuple(self.hdr_train[0][0].shape)
+
+        if stage == "test" or stage is None:
+            self.hdr_test = LUTDataset(
+                df=self.df,
+                exposure_path=(self.data_dir / "correct_exposures" / "exposures"),
+                raw_dir=self.data_dir / "dngs",
+                name_list=self.precomputed_data_dir / "test_current.csv",
+                transform=self.transform,
+            )
+            self.dims = tuple(self.hdr_test[0][0].shape)
+
+    def train_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
+        return DataLoader(
+            self.hdr_train,
+            batch_size=self.batch_size,
+            num_workers=8,
+            pin_memory=True,
+            shuffle=True,
+        )
+
+    def val_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
+        return DataLoader(
+            self.hdr_val, batch_size=self.batch_size, pin_memory=True, num_workers=8
+        )
+
+    def test_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
+        return DataLoader(
+            self.hdr_test, batch_size=self.batch_size, pin_memory=True, num_workers=8
+        )
--- dhdrnet/model.py
+++ dhdrnet/model.py
@@ -1,45 +1,30 @@
-from math import ceil
-from typing import Dict, List, Union
+from typing import Dict, List, Union, Callable
 
-import pandas as pd
 import torch
 import torch.nn as nn
 from pytorch_lightning import LightningModule
 from torch import Tensor
 from torch.optim import Adam
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-from torch.utils.data import DataLoader, random_split
-from torchsummary import summary
 from torchvision import models
-from torchvision.transforms import (
-    Compose,
-    Resize,
-    ToTensor,
-)
 
 from dhdrnet.Dataset import LUTDataset
 from dhdrnet.squeezenet import squeezenet1_1
-from dhdrnet.util import DATA_DIR, ROOT_DIR
 
 
 class DHDRNet(LightningModule):
     def __init__(
-        self, learning_rate=1e-3, batch_size=8, want_summary=False, *args, **kwargs
+        self,
+        learning_rate=1e-3,
+        batch_size=8,
     ):
-        super().__init__(*args, **kwargs)
+        super().__init__()
         self.num_classes = 36
         self.learning_rate = learning_rate
+        self.criterion: Callable
         self.batch_size = batch_size
         self.Dataset = LUTDataset
         self.save_hyperparameters()
-        self.transform = Compose(
-            [
-                Resize((300, 300)),
-                # RandomHorizontalFlip(p=0.5),
-                # RandomVerticalFlip(p=0.5),
-                ToTensor(),
-            ]
-        )
 
     def print_summary(self, model, size):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -47,90 +32,32 @@ class DHDRNet(LightningModule):
         summary(model, (3, 300, 300))
         del model
 
-    def prepare_data(self):
-        data_df = pd.read_csv(ROOT_DIR / "precomputed_data" / "store_current.csv")
-        trainval_data = self.Dataset(
-            df=data_df,
-            exposure_path=DATA_DIR / "correct_exposures" / "exposures",
-            raw_dir=DATA_DIR / "dngs",
-            name_list=ROOT_DIR / "precomputed_data" / "train_current.csv",
-            transform=self.transform,
-        )
-        test_data = self.Dataset(
-            df=data_df,
-            exposure_path=DATA_DIR / "correct_exposures" / "exposures",
-            raw_dir=DATA_DIR / "dngs",
-            name_list=ROOT_DIR / "precomputed_data" / "test_current.csv",
-            transform=self.transform,
-        )
-        train_val_ratio = 0.9
-        train_len = ceil(train_val_ratio * len(trainval_data))
-        val_len = len(trainval_data) - train_len
-        train_data, val_data = random_split(trainval_data, lengths=(train_len, val_len))
-        self.train_data = train_data
-        self.val_data = val_data
-        self.test_data = test_data
-        self.generator = trainval_data.generator
-
-    def train_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
-        return DataLoader(
-            self.train_data,
-            batch_size=self.batch_size,
-            num_workers=8,
-            pin_memory=True,
-            shuffle=True,
-        )
-
-    def val_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
-        return DataLoader(
-            self.val_data, batch_size=self.batch_size, pin_memory=True, num_workers=8
-        )
-
-    def test_dataloader(self) -> Union[DataLoader, List[DataLoader]]:
-        return DataLoader(
-            self.test_data, batch_size=self.batch_size, pin_memory=True, num_workers=8
-        )
-
     def configure_optimizers(self):
         optimizer = Adam(self.parameters(), lr=self.learning_rate, weight_decay=1e-5)
         scheduler = ReduceLROnPlateau(optimizer)
         return [optimizer], [scheduler]
 
     def common_step(self, batch):
-        mid_exposure, label, stats = batch
+        mid_exposure, label = batch
         outputs = self(mid_exposure)
 
         loss = self.criterion(outputs, label)
         return loss
 
-    def training_step(self, batch, batch_idx) -> Dict[str, Union[Dict, Tensor]]:
+    def training_step(self, batch) -> Tensor:
         loss = self.common_step(batch)
-        logs = {"train_loss": loss}
-        return {"loss": loss, "log": logs}
+        self.log("training_loss", loss)
+        return loss
 
-    def validation_step(self, batch, batch_idx) -> Dict[str, Union[Dict, Tensor]]:
+    def validation_step(self, batch) -> Tensor:
         loss = self.common_step(batch)
-        logs = {"val_loss": loss}
-        return {"val_loss": loss, "log": logs}
+        self.log("val_loss", loss)
+        return loss
 
-    def validation_epoch_end(
-        self,
-        outputs: List[Dict[str, Tensor]],
-    ) -> Dict[str, Union[Dict, Tensor]]:
-        avg_loss: Tensor = torch.stack([x["val_loss"] for x in outputs]).mean()
-        tensorboard_logs: Dict[str, Tensor] = {"val_loss": avg_loss}
-        logs = {"val_loss": avg_loss, "log": tensorboard_logs}
-        return logs
-
-    def test_step(self, batch, batch_idx) -> Dict[str, Union[Dict, Tensor]]:
+    def test_step(self, batch) -> Tensor:
         loss = self.common_step(batch)
-        logs = {"test_loss": loss}
-        return {"test_loss": loss, "log": logs}
-
-    def test_epoch_end(self, outputs) -> Dict[str, Union[Dict, Tensor]]:
-        avg_loss = torch.stack([x["test_loss"] for x in outputs]).mean()
-        tensorboard_logs = {"test_loss": avg_loss}
-        return {"test_loss": avg_loss, "log": tensorboard_logs}
+        self.log("test_loss", loss)
+        return loss
 
 
 class DHDRMobileNet_v1(DHDRNet):
@@ -197,7 +124,7 @@ class DHDRSqueezeNet(DHDRNet):
         self.inner_model = squeezenet1_1(pretrained=False, num_classes=self.num_classes)
 
         if want_summary:
-            super().print_summary(self.inner_model, size=(1, 300, 300))
+            self.print_summary(self.inner_model, size=(1, 300, 300))
 
         self.criterion = nn.CrossEntropyLoss()
 
@@ -213,7 +140,7 @@ class DHDRSimple(DHDRNet):
         self.model = self._simple_model(self.num_classes)
 
         if want_summary:
-            super().print_summary(self.model, size=(1, 300, 300))
+            self.print_summary(self.model, size=(1, 300, 300))
 
         self.criterion = nn.CrossEntropyLoss()
 
