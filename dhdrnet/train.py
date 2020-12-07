5c5098d7acd85b99d1f2396315f802b9ad144821
Shane Segal <shane@smsegal.ca>
2020-12-07 15:55:22 -0500

    move some files to precomputed_data submodule

--- dhdrnet/train.py
+++ dhdrnet/train.py
@@ -1,102 +1,68 @@
-import datetime
-from argparse import ArgumentParser
-
-from pytorch_lightning import Trainer, loggers
-from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
-
-from dhdrnet import histogram_model
+from dhdrnet.data_module import HDRDataModule
+from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
+import fire
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from dhdrnet.histogram_model import HistogramNet
+from typing import Dict, Optional
+from pathlib import Path
 from dhdrnet import model as models
 from dhdrnet import resnet_model
 from dhdrnet import reconstruction_model
-from dhdrnet.util import ROOT_DIR
+import pytorch_lightning as pl
 
 
-def main(hparams=None):
-    if hparams.backbone == "squeeze":
-        Model = models.DHDRSqueezeNet
-    elif hparams.backbone == "mobile_v1":
-        Model = models.DHDRMobileNet_v1
-    elif hparams.backbone == "mobile_v2":
-        Model = models.DHDRMobileNet_v2
-    elif hparams.backbone == "mobile_v3":
-        Model = models.DHDRMobileNet_v3
-    elif hparams.backbone == "simple":
-        Model = models.DHDRSimple
-    elif hparams.backbone == "hist":
-        Model = histogram_model.HistogramNet
-    elif hparams.backbone == "resnet":
-        Model = resnet_model.DHDRResnet
-    elif hparams.backbone == "reconstruction":
-        Model = reconstruction_model.RCNet
+ModelClass: Dict[str, models.DHDRNet] = {
+   "squeeze": models.DHDRSqueezeNet,
+   "mobile_v1": models.DHDRMobileNet_v1,
+   "mobile_v2": models.DHDRMobileNet_v2,
+   "mobile_v3": models.DHDRMobileNet_v3,
+   "simple": models.DHDRSimple,
+   "hist": HistogramNet,
+   "resnet": resnet_model.DHDRResnet,
+   "reconstruction": reconstruction_model.RCNet,
+}
 
-    checkpoint_path = hparams.checkpoint_path
+
+def train(
+    backbone: str,
+    checkpoint_path: Path,
+    batch_size: int = 20,
+    learning_rate: float = 1e-3,
+    gpus: int = 1,
+    resume_path: Optional[Path] = None,
+    summarize: bool = False,
+    auto_scale_batch: Optional[str] = None,
+    auto_learning_rate: bool = False,
+    stage: str = "train",
+):
+    Model = ModelClass[backbone]
     model = Model(
-        batch_size=int(hparams.batch_size),
-        learning_rate=int(hparams.learning_rate),
-        want_summary=hparams.summary,
+        batch_size=batch_size, learning_rate=learning_rate, want_summary=summarize
     )
 
-    timestamp = datetime.datetime.now().isoformat()
-    logger = loggers.TensorBoardLogger(
-        ROOT_DIR / "logs", name=f"dhdr_{hparams.backbone}"
+    data_module = HDRDataModule()
+
+    trainer = pl.Trainer(
+        gpus=gpus,
+        auto_lr_find=auto_learning_rate,
+        auto_scale_batch_size=auto_scale_batch,
+        checkpoint_callback=True,
+        callbacks=[
+            EarlyStopping(monitor="val_loss", patience=15),
+            ModelCheckpoint(
+                dirpath=Path(checkpoint_path) / backbone,
+                save_last=True,
+                monitor="val_loss"
+            ),
+        ],
+        resume_from_checkpoint=resume_path,
     )
 
-    checkpoint_loss_callback = ModelCheckpoint(
-        monitor="val_loss",
-        mode="min",
-        save_top_k=3,
-        prefix=hparams.backbone,
-        filepath=ROOT_DIR / "checkpoints" / "dhdr-{epoch}-{val_loss:.2f}",
-    )
-
-    early_stopping = EarlyStopping("val_loss", patience=15)
-
-    trainer = Trainer(
-        gpus=hparams.gpus,
-        logger=logger,
-        weights_summary=None,
-        auto_lr_find=hparams.auto_lr,
-        auto_scale_batch_size="binsearch" if hparams.auto_batch else None,
-        checkpoint_callback=checkpoint_loss_callback,
-        early_stop_callback=early_stopping,
-        resume_from_checkpoint=checkpoint_path if checkpoint_path else None,
-        max_epochs=2000,
-    )
-
-    if not hparams.test_only:
-        trainer.fit(model)
-        trainer.save_checkpoint(
-            ROOT_DIR / f"checkpoints/dhdr_{hparams.backbone}_final{timestamp}.ckpt"
-        )
+    if stage == "train":
+        trainer.fit(model, datamodule=data_module)
 
     trainer.test()
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument("--gpus", default=None)
-    parser.add_argument(
-        "--backbone",
-        choices=[
-            "mobile_v1",
-            "mobile_v2",
-            "mobile_v3",
-            "squeeze",
-            "simple",
-            "hist",
-            "resnet",
-            "reconstruction",
-        ],
-    )
-    parser.add_argument("--test-only", action="store_true")
-    parser.add_argument("-c", "--checkpoint-path", default=None)
-    parser.add_argument(
-        "--summary", help="print a summary of model weights", action="store_true"
-    )
-    parser.add_argument("--auto-batch", action="store_true")
-    parser.add_argument("--batch-size", default=24)
-    parser.add_argument("--auto-lr", action="store_true")
-    parser.add_argument("--learning-rate", "-l", default=1e-7)
-    args = parser.parse_args()
-    print(args)
-    main(args)
+    fire.Fire(train)
