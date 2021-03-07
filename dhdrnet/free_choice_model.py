2d83aa3c23b6bc1c6309c76321f9e6ce25d4685b
Shane Segal <shane@smsegal.ca>
2021-03-07 17:44:38 -0500

    training appears to work on CPU with a small dataset, time to test on AWS with a GPU

--- dhdrnet/free_choice_model.py
+++ dhdrnet/free_choice_model.py
@@ -1,7 +1,8 @@
 from functools import partial
+from pathlib import Path
 import time
 from multiprocessing import cpu_count
-from typing import Callable, List, Optional, Tuple
+from typing import Callable, List, Optional, Tuple, Union
 
 import timm
 import torch
@@ -11,13 +12,14 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.tensor import Tensor
 from torch.utils.data import DataLoader, Dataset
 from torch.utils.tensorboard import SummaryWriter
+from tqdm import trange, tqdm
 
 
 class FreeChoiceDHDRNet(nn.Module):
     """num_classes will be n choose 2 for n = number of exposure values in consideration"""
 
     def __init__(self, num_classes: int):
-        super(FreeChoiceDHDRNet, self).__init__()
+        super().__init__()
         backbone = timm.create_model(
             "mobilenetv3_large_100",
             pretrained=True,
@@ -37,10 +39,8 @@ class FreeChoiceDHDRNet(nn.Module):
         self.inconv = nn.Conv3d(in_channels=7, out_channels=1, kernel_size=3, padding=1)
 
     def forward(self, x):
-        # print(f"input shape = {x.shape}")
         x = self.inconv(x)
         x = torch.squeeze(x)
-        # print(f"output shape = {x.shape}")
         x = self.backbone(x)
         return x
 
@@ -55,7 +55,7 @@ def train_epoch(
     val_steps: int,  # validate every val_steps steps
     writer: SummaryWriter,
     use_cuda: bool = False,
-) -> Tuple[torch.Tensor, List]:
+) -> Tuple[torch.Tensor, List, float]:
     model.train()
     time_start = time.time()
     last_idx = len(loader)
@@ -63,7 +63,8 @@ def train_epoch(
     train_losses = []
     val_losses = []
     val_counter = 0
-    for idx, (input, target, score) in enumerate(loader):
+    train_iter = tqdm(loader, desc="Train", leave=False)
+    for idx, (input, target, score) in enumerate(train_iter):
         last_batch = idx == last_idx
 
         if use_cuda:
@@ -76,20 +77,23 @@ def train_epoch(
         loss.backward()
         train_losses.append(loss)
         optimizer.step()
-        writer.add_scalar("Loss/train", loss, idx)
+        writer.add_scalar("Loss/train", loss)
 
         if use_cuda:
             torch.cuda.synchronize()
 
+        train_duration = time_start - time.time()
+
+        train_iter.set_postfix({"train_loss": loss.item(), "batch_idx": idx})
         if idx % val_steps == 0:
             val_loss, val_duration = validate(model=model, use_cuda=use_cuda)
-            writer.add_scalar("Loss/val", val_loss, val_counter)
+            writer.add_scalar("Loss/val", val_loss)
             val_losses.append(val_loss)
             val_counter += 1
 
-    epoch_duration = time_start - time.time()
+    epoch_duration = time.time() - time_start
     epoch_train_loss = torch.mean(torch.tensor(train_losses))
-    return epoch_train_loss, val_losses
+    return epoch_train_loss, val_losses, epoch_duration
 
 
 def validate(
@@ -99,20 +103,25 @@ def validate(
     with torch.no_grad():
         val_loss = []
         time_start = time.time()
-        for idx, (input, target, score) in enumerate(loader):
+        duration = 0
+        val_iter = tqdm(loader, desc="Val", leave=False)
+        for idx, (input, target, score) in enumerate(val_iter):
             if use_cuda:
                 input, target = input.cuda(), target.cuda()
             output = model(input)
             loss = loss_fn(output, target)
             val_loss.append(loss)
-        duration = time_start - time.time()
+
+            val_iter.set_postfix({"val_loss": loss.item(), "batch_idx": idx})
+
+        duration = time.time() - time_start
         avg_val_loss = torch.mean(torch.tensor(val_loss))
     model.train()
     scheduler.step(avg_val_loss)
     return avg_val_loss, duration
 
 
-def train_model(
+def fit(
     model: nn.Module,
     train_ds: Dataset,
     val_ds: Dataset,
@@ -120,6 +129,7 @@ def train_model(
     num_epochs: int,
     learning_rate: Optional[float] = None,
     exp_name: str = "0",
+    logdir: Union[Path, str] = "../logs",
 ):
     optimizer = Adam(
         model.parameters(),
@@ -128,7 +138,7 @@ def train_model(
     scheduler = ReduceLROnPlateau(optimizer)
     loss_fn = nn.CrossEntropyLoss()
 
-    writer = SummaryWriter(log_dir=f"logs/exp_{exp_name}")
+    writer = SummaryWriter(log_dir=f"{logdir}/exp_{exp_name}")
 
     train_loader = DataLoader(
         train_ds,
@@ -139,14 +149,18 @@ def train_model(
     )
     val_loader = DataLoader(
         val_ds,
-        batch_size=batch_size // 2,
-        shuffle=True,
+        batch_size=batch_size * 2,
+        shuffle=False,
         num_workers=cpu_count(),
         pin_memory=True,
     )
 
-    for epoch in range(num_epochs):
-        epoch_loss, val_losses = train_epoch(
+    epoch_iter = trange(
+        num_epochs,
+        desc="Epochs",
+    )
+    for epoch in epoch_iter:
+        epoch_loss, val_losses, epoch_duration = train_epoch(
             epoch,
             model,
             train_loader,
@@ -161,6 +175,10 @@ def train_model(
             ),
             use_cuda=torch.cuda.is_available(),
             writer=writer,
+        )
+
+        epoch_iter.set_postfix(
+            {"epoch_train_loss": epoch_loss.item(), "epoch_duration": epoch_duration},
         )
         writer.add_scalar("Loss/epoch_train", epoch_loss, epoch)
         writer.add_scalar("Loss/epoch_val", torch.mean(torch.tensor(val_losses)), epoch)
